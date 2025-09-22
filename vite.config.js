fcd6bd9b250ae04a41f9537a359c1c21697e1024
Swaviman2004 <162682906+Swaviman2004@users.noreply.github.com>
2025-09-22 20:26:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this line to set the base path for your application
+  base: '/ourstore/',
 })
