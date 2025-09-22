d10c481b231be6684931b22fbb34ac276c02fc83
Swaviman2004 <162682906+Swaviman2004@users.noreply.github.com>
2025-09-23 01:33:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const [feedback, setFeedback] = useState('');
 
   // Simulating API calls
-  const API_BASE_URL = 'http://localhost:8087/api';
+  const API_BASE_URL = 'http://localhost:8083/api';
 
   useEffect(() => {
     // Fetch products when the component mounts
